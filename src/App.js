1c1f0e8f42c81a6d19654e763aaf603f7d59794e
Augie Bren <Augie>
2022-06-23 21:14:17 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
 import './App.css';
-
+import Button from 'react-bootstrap/Button'
+import Nav from 'react-bootstrap/Nav'
+import Form from 'react-bootstrap/Form'
 
 
 function App() {
   return (
     <div className="App">
       <h1>Bootstrap Website</h1>
+      <Nav activeKey="/home" onSelect={(selectedKey) => alert(`selected ${selectedKey}`)}>
+        <Nav.Item>
+          <Nav.Link href="/home">Active</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-1">Link</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="link-2">Link2</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link eventKey="disabled" disabled>Disabled</Nav.Link>
+        </Nav.Item>
+      </Nav>
+      
+      <Button variant="primary">Button #1</Button>
+
+      <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+      </Form>
+
+
+      <Form.Group>
+        <Form.Control as="select">
+          <option>Inception</option>
+          <option>Frost</option>
+          <option>warm</option>
+          <option>The best Choice</option>
+        </Form.Control>
+      </Form.Group>
      
     </div>
   );
